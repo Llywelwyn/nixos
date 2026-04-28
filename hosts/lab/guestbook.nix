0c82c86c5036ccf02d88ecfe0b6570a5e57ad194
lew <lew@ily.rs>
2026-04-28 16:27:07 +0100

    chore: update to new guestbook module style

--- hosts/lab/guestbook.nix
+++ hosts/lab/guestbook.nix
@@ -25,34 +25,42 @@
       };
     };
 
-    features = {
-      submissions.enable = true;
-      websites.enable = true;
-      drawing = {
-        enable = true;
-        canvasWidth = 300;
-        canvasHeight = 140;
-      };
-      voiceNote = {
-        enable = true;
-        maxDuration = 20;
-      };
-      telegram = {
-        enable = true;
-        chatId = 8669496383;
-        botTokenFile = config.sops.secrets.telegram_bot_token.path;
-      };
-      security = {
-        htmlInjection.enable = true;
-        honeypot.enable = true;
-        captcha.enable = false;
-      };
+    submissions.enable = true;
+    websites.enable = true;
+    content.required = false;
+    message.required = false;
+
+    drawing = {
+      enable = true;
+      required = false;
+    };
+
+    voice = {
+      enable = true;
+      required = false;
+    };
+
+    telegram = {
+      enable = true;
+      chatId = 8669496383;
+      botTokenFile = config.sops.secrets.telegram_bot_token.path;
+    };
+
+    security = {
+      htmlInjection.enable = true;
+      honeypot.enable = true;
+      captcha.enable = false;
     };
 
     limits = {
-      name = 0;
-      message = 0;
-      website = 0;
+      name.length = 0;
+      message.length = 0;
+      website.length = 0;
+      drawing = {
+        width = 300;
+        height = 140;
+      };
+      voice.duration = 20;
     };
 
     styles = {
