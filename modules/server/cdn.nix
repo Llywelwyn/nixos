5a64d8ebbc82d9f667f37ec89bfa91e635494901
lew <git@lewis.wales>
2026-07-10 20:01:31 +0100

    cdn: add uptime probe

--- modules/server/cdn.nix
+++ modules/server/cdn.nix
@@ -1,5 +1,7 @@
 {
   flake.modules.nixos.server = {
+    services.uptime-page.probes.cdn = { url = "https://cdn.ily.rs"; order = 130; };
+
     services.caddy.virtualHosts."cdn.ily.rs" = {
       extraConfig = ''
         header Access-Control-Allow-Origin "*"
