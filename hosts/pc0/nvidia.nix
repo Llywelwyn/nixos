a128d1e9e79b8328884c1dff860dbe2829e77604
Lewis Wynne <l@ily.rs>
2026-07-10 17:46:25 +0100

    pc0: pin legacy_500 driver

--- hosts/pc0/nvidia.nix
+++ hosts/pc0/nvidia.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
   services.xserver.videoDrivers = [ "nvidia" ];
 
@@ -6,6 +6,9 @@
     modesetting.enable = true;
     nvidiaSettings = true;
     open = false; # GTX 1080 is unsupported
+    # GTX 1080 (Pascal) was dropped from the mainline driver (595+);
+    # it now needs the 580 legacy branch.
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
   };
 
   hardware.graphics = {
