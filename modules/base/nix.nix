b02af7fc1f51240ebec6e3f54f3f00cac15be0b9
lew <git@lewis.wales>
2026-07-10 19:58:08 +0100

    nix: drop channels, pin registry and NIX_PATH to flake nixpkgs

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -1,6 +1,10 @@
+{ inputs, ... }:
 {
   flake.modules.nixos.base = {
     nix = {
+      channel.enable = false;
+      registry.nixpkgs.flake = inputs.nixpkgs;
+      nixPath = [ "nixpkgs=flake:nixpkgs" ];
       settings.experimental-features = [ "nix-command" "flakes" ];
       settings.auto-optimise-store = true;
       gc = {
