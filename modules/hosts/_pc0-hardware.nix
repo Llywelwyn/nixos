a238a059abcdeecfbacc6170544c14237f5546df
Lewis Wynne <l@ily.rs>
2026-07-30 14:25:09 +0100

    pc0: 8G disk swapfile on /persist to back up zram

--- modules/hosts/_pc0-hardware.nix
+++ modules/hosts/_pc0-hardware.nix
@@ -42,7 +42,12 @@
     };
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/persist/swapfile";
+      size = 8 * 1024;
+    }
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
