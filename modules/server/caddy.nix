a301043c697cbebaf2e7bf48b52e29c0557a1ae8
Lewis Wynne <l@ily.rs>
2026-09-01 23:01:44 +0100

    caddy: limit rolling logs

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -1,13 +1,31 @@
 { config, ... }:
 {
-  flake.modules.nixos.server = {
-    services = {
-      caddy = {
-        enable = true;
-        email = config.flake.meta.email;
-      };
-      telegram-alerts.units = [ "caddy" ];
+  flake.modules.nixos.server = { lib, ... }: {
+    options.services.caddy.virtualHosts = lib.mkOption {
+      type = lib.types.attrsOf (
+        lib.types.submodule (
+          { config, ... }:
+          {
+            logFormat = lib.mkDefault ''
+              output file /var/log/caddy/access-${config.hostName}.log {
+                roll_size 10MiB
+                roll_keep 3
+                roll_keep_for 30d
+              }
+            '';
+          }
+        )
+      );
     };
-    networking.firewall.allowedTCPPorts = [ 80 443 ];
+    config = {
+      services = {
+        caddy = {
+          enable = true;
+          email = config.flake.meta.email;
+        };
+        telegram-alerts.units = [ "caddy" ];
+      };
+      networking.firewall.allowedTCPPorts = [ 80 443 ];
+    };
   };
 }
