ac97fed6a37c3b3be1007ad12c82bbdf22ebdee8
Lewis Wynne <l@ily.rs>
2026-09-03 19:22:55 +0100

    meta: drop the option boilerplate

--- modules/meta.nix
+++ modules/meta.nix
@@ -1,21 +1,5 @@
-{ lib, flake-parts-lib, ... }:
 {
-  options.flake = flake-parts-lib.mkSubmoduleOptions {
-    meta = lib.mkOption {
-      type = lib.types.submodule {
-        options.username = lib.mkOption {
-          type = lib.types.str;
-          description = "Primary user account name.";
-        };
-        options.email = lib.mkOption {
-          type = lib.types.str;
-          description = "Primary contact email.";
-        };
-      };
-    };
-  };
-
-  config.flake.meta = {
+  flake.meta = {
     username = "l";
     email = "l@ily.rs";
   };
