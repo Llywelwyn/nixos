2695aefce7f704b0efef231e3063230f1083d91a
Lewis Wynne <l@ily.rs>
2026-09-03 19:22:55 +0100

    users: inline the ssh keys

--- keys/default.nix
+++
@@ -1,4 +0,0 @@
-{
-  pc0 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETmIpkPkDLn21FuEA6n8WEQLzM3bfbMwiknkDtRbMhG pc0";
-  pc1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfukxi9IGnne4uEQqGHER6d4d0HLq8flJApeqt/Lkag lew@ily.rs";
-}
--- modules/base/users.nix
+++ modules/base/users.nix
@@ -4,7 +4,10 @@
     users.users.${config.flake.meta.username} = {
       isNormalUser = true;
       extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = builtins.attrValues (import ../../keys);
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETmIpkPkDLn21FuEA6n8WEQLzM3bfbMwiknkDtRbMhG pc0"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfukxi9IGnne4uEQqGHER6d4d0HLq8flJApeqt/Lkag lew@ily.rs"
+      ];
     };
   };
 }
