a917fba52cea1c12d8600d8f3a82f809077aa4ce
Lewis Wynne <l@ily.rs>
2026-09-03 19:22:49 +0100

    flake: remove nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
